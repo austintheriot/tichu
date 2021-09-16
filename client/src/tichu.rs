823e213af6a97e59edbbfeaa29c59e01f660862c
Austin Theriot <austinmtheriot@gmail.com>
2021-09-15 21:44:16 -0500

    clean up WIP

--- client/src/tichu.rs
+++ client/src/tichu.rs
@@ -2,38 +2,32 @@ use anyhow::Error;
 use bincode;
 use common::CTSMsg;
 use log::*;
-use yew::binary_format;
-use yew::format::Bincode;
 use yew::format::{Binary, Json};
 use yew::prelude::*;
 use yew::services::websocket::{WebSocketService, WebSocketStatus, WebSocketTask};
-use yew::services::ConsoleService;
 
 pub struct App {
     ws: Option<WebSocketTask>,
     link: ComponentLink<Self>,
-    text: String,
     server_data: String,
 }
 
-pub enum Msg {
-    Connect,
+pub enum AppMsg {
+    ConnectToWS,
     Disconnected,
-    Ignore,
-    TextInput(String),
-    Received(Result<Vec<u8>, Error>),
-    SendMsg(CTSMsg),
+    Noop,
+    WSMsgReceived(Result<Vec<u8>, Error>),
+    SendWSMsg(CTSMsg),
 }
 
 impl Component for App {
-    type Message = Msg;
+    type Message = AppMsg;
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             ws: None,
             link: link,
-            text: String::new(),
             server_data: String::new(),
         }
     }
@@ -41,23 +35,27 @@ impl Component for App {
     fn rendered(&mut self, first_render: bool) {
         // connect to websocket on first render
         if self.ws.is_none() && first_render {
-            info!("Sending automatic message to connect");
-            self.link.send_message(Msg::Connect);
+            self.link.send_message(AppMsg::ConnectToWS);
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::Connect => {
+            AppMsg::Noop => false,
+            AppMsg::Disconnected => {
+                self.ws = None;
+                false
+            }
+            AppMsg::ConnectToWS => {
                 info!("Connecting to websocket...");
                 let handle_ws_receive_data = self
                     .link
-                    .callback(|data: Result<Vec<u8>, Error>| Msg::Received(data));
+                    .callback(|data: Result<Vec<u8>, Error>| AppMsg::WSMsgReceived(data));
                 let handle_ws_update_status = self.link.callback(|ws_status| {
                     info!("Websocket status: {:?}", ws_status);
                     match ws_status {
-                        WebSocketStatus::Closed | WebSocketStatus::Error => Msg::Disconnected,
-                        WebSocketStatus::Opened => Msg::Ignore,
+                        WebSocketStatus::Closed | WebSocketStatus::Error => AppMsg::Disconnected,
+                        WebSocketStatus::Opened => AppMsg::Noop,
                     }
                 });
                 if self.ws.is_none() {
@@ -70,42 +68,46 @@ impl Component for App {
                 }
                 true
             }
-            Msg::Disconnected => {
-                self.ws = None;
-                true
-            }
-            Msg::Ignore => false,
-            Msg::TextInput(e) => {
-                self.text = e;
-                true
-            }
-            Msg::SendMsg(msg_type) => {
+            AppMsg::SendWSMsg(msg_type) => {
                 match self.ws {
                     None => {
                         info!("Can't send message. Websocket is not connected.");
                     }
-                    Some(ref mut ws_task) => match msg_type {
+                    Some(ref mut ws_task) => {
+                        info!("Sending websocket message: {:?}", &msg_type);
+                        match msg_type {
                         CTSMsg::Test(s) => {
-                            info!("Sending websocket message: {:?}", &s);
                             let s = bincode::serialize(&CTSMsg::Test(s))
+                                .expect("Could not serialize message");
+                            ws_task.send_binary(Binary::Ok(s));
+                        }
+                        CTSMsg::Ping => {
+                            let s = bincode::serialize(&CTSMsg::Ping)
                                 .expect("Could not serialize message");
                             ws_task.send_binary(Binary::Ok(s));
                         }
                         _ => {
                             info!("Unexpected message type received {:?}", &msg_type);
                         }
-                    },
+                    }},
                 }
                 false
             }
-            Msg::Received(data) => {
+            AppMsg::WSMsgReceived(data) => {
                 if data.is_err() {
-                    info!("Data received from server was an error {:?}", &data);
+                    info!("Data received from websocket was an error {:?}", &data);
                     return false;
                 }
                 let data: CTSMsg = bincode::deserialize(&data.unwrap())
-                    .expect("Could not deserialize message from server");
+                    .expect("Could not deserialize message from websocket");
+                info!("Received websocket message: {:?}", &data);
                 match data {
+                    CTSMsg::Ping => {
+                        self.link.send_message(AppMsg::SendWSMsg(CTSMsg::Pong));
+                    }
+                    CTSMsg::Pong => {
+                        info!("Pong received from websocket!")
+                    }
                     CTSMsg::Test(string) => {
                         info!("Test message received! Message: {}", string);
                     }
@@ -124,8 +126,9 @@ impl Component for App {
         html! {
             <div>
                 <p>{ "Websocket status: "}{ if self.ws.is_none() {"Connecting..."} else { "Connected" }} </p>
-                <button onclick=self.link.callback(|_| Msg::SendMsg(CTSMsg::Test(String::from("Hello server!"))))>{ "Send message to server" }</button>
-                // <input type="text" value=self.text.clone() oninput=self.link.callback(|e: InputData| Msg::TextInput(e.value))/>
+                <button onclick=self.link.callback(|_| AppMsg::SendWSMsg(CTSMsg::Test(String::from("Hello server!"))))>{ "Send test message to server" }</button>
+                <button onclick=self.link.callback(|_| AppMsg::SendWSMsg(CTSMsg::Ping))>{ "Send ping to server" }</button>
+                // <input type="text" value=self.text.clone() oninput=self.link.callback(|e: InputData| AppMsg::TextInput(e.value))/>
                 <p>{ "Message received from server:" }</p>
                 <textarea value=self.server_data.clone()></textarea>
             </div>
