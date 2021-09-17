9dd8203fb36340b05fe91ac46321c79c1ba3e700
Austin Theriot <austinmtheriot@gmail.com>
2021-09-17 06:15:46 -0500

    everything working so far (WIP)

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
-pub const NO_ID: &str = "no_id";
+pub const NO_USER_ID: &str = "NO_USER_ID";
+pub const NO_GAME_UD: &str = "NO_GAME_UD";
 
 #[derive(Debug, Serialize, Deserialize)]
 enum TichuCallStatus {
