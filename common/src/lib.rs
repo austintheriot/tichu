79350f6447b8e70f2829b4919bd78005302e130f
Austin Theriot <austinmtheriot@gmail.com>
2021-09-18 07:22:47 -0500

    validate user-game-id-relationship WIP

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,49 +1,85 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 pub const NO_USER_ID: &str = "NO_USER_ID";
 pub const NO_GAME_UD: &str = "NO_GAME_UD";
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 enum TichuCallStatus {
     Called,
     Achieved,
     Failed,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum GameStage {
     Join,
-    Lobby,
+    Lobby(LobbyStage),
+    Teams,
     GrandTichu,
     Trade,
     Game,
     Endgame,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Game {
-    id: String,
-    stage: GameStage,
-    small_tichus: Vec<TichuCallStatus>,
-    grand_tichus: Vec<TichuCallStatus>,
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct LobbyStage {
     participants: Vec<User>,
-    teams: [Team; 2],
     owner_id: String,
-    active_player: String,
-    card_wished_for: Card,
-    deck: Vec<Card>,
-    discard: Vec<Card>,
-    in_play: Vec<Card>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct GameState {
+    id: String,
+    stage: GameStage,
+    // small_tichus: Vec<TichuCallStatus>,
+    // grand_tichus: Vec<TichuCallStatus>,
+    // teams: [Team; 2],
+    // owner_id: String,
+    // active_player: String,
+    // card_wished_for: Card,
+    // deck: Vec<Card>,
+    // discard: Vec<Card>,
+    // in_play: Vec<Card>,
+}
+
+impl GameState {
+    pub fn id(&self) -> String {
+        self.id.clone()
+    }
+
+    pub fn stage(&self) -> GameStage {
+        self.stage.clone()
+    }
+
+    pub fn new(owner_id: String, owner_display_name: String) -> GameState {
+        let owner_user = User {
+            display_name: owner_display_name.clone(),
+            id: owner_id.clone(),
+            role: UserRole::Owner,
+            tricks: vec![],
+            hand: vec![],
+        };
+        let lobby_stage = LobbyStage {
+            owner_id: owner_id.clone(),
+            participants: vec![owner_user],
+        };
+        let game_state = GameState {
+            id: Uuid::new_v4().to_string(),
+            stage: GameStage::Lobby(lobby_stage),
+        };
+        game_state
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct Team {
     id: String,
     users: Vec<String>,
     score: i32,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum CardValue {
     _2,
     _3,
@@ -59,7 +95,7 @@ pub enum CardValue {
     K,
     A,
 }
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum Card {
     Sword(CardValue),
     Jade(CardValue),
@@ -71,29 +107,20 @@ pub enum Card {
     Dog,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct Trade {
     from: String,
     to: String,
     card: Card,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-enum Stage {
-    Lobby,
-    Teams,
-    Trade,
-    Game,
-    Postgame,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum UserRole {
     Owner,
     Participant,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct User {
     id: String,
     role: UserRole,
@@ -101,59 +128,58 @@ pub struct User {
     hand: Vec<Card>,
     tricks: Vec<Vec<Card>>,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CreateGameMessage {
-    user_id: String,
-    display_name: String,
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct CreateGame {
+    pub user_id: String,
+    pub display_name: String,
+}
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct JoinGameWithGameId {
+    pub user_id: String,
+    pub display_name: String,
+    pub game_id: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct JoinGameMessage {
-    user_id: String,
-    display_name: String,
-    game_id: String,
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct JoinRandomGame {
+    pub user_id: String,
+    pub display_name: String,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub struct ChooseTeamMessage {
-    team_id: String,
+    pub team_id: String,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct RenameTeamMessage {
-    team_name: String,
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct RenameTeam {
+    pub team_name: String,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct SubmitTradeMessage {
-    trade_to: String,
-    card: Card,
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct SubmitTrade {
+    pub trade_to: String,
+    pub card: Card,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct PlayCardsMessage {
-    cards: Vec<Card>,
-    wished_for: Option<Card>,
-    give_dragon_to: Option<String>,
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct PlayCard {
+    pub cards: Vec<Card>,
+    pub wished_for: Option<Card>,
+    pub give_dragon_to: Option<String>,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct GiveDragonMessage {
-    user_id: String,
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct GiveDragon {
+    pub user_id: String,
 }
 
 /// Client to Server Websocket Messages
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum CTSMsg {
-    CreateGame(CreateGameMessage),
-    JoinGame(JoinGameMessage),
-    ChooseTeam(ChooseTeamMessage),
-    RenameTeam(RenameTeamMessage),
+    JoinGameWithGameId(JoinGameWithGameId),
+    JoinRandomGame(JoinRandomGame),
+    CreateGame(CreateGame),
+    RenameTeam(RenameTeam),
     StartGame,
-    SubmitTrade(SubmitTradeMessage),
-    PlayCards(PlayCardsMessage),
-    GiveDragon(GiveDragonMessage),
+    SubmitTrade(SubmitTrade),
+    PlayCards(PlayCard),
+    GiveDragon(GiveDragon),
 
     Ping,
     Pong,
@@ -161,13 +187,14 @@ pub enum CTSMsg {
 }
 
 /// Server to Client Websocket Messages
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum STCMsg {
-    State(Game),
     UserIdAssigned(String),
     GameCreated,
+    GameState(GameState),
     GameStateChanged,
     TeamRenamed,
+    UserJoined(String),
     SmallTichuCalled,
     GrandTichuCalled,
     StartGame,
@@ -180,7 +207,8 @@ pub enum STCMsg {
     CardsPlayed,
     DragonWasWon,
     PlayerReceivedDragon,
-    EndGame,
+    EndGame, // show scores, etc.
+    GameOver, // game state has been entirely cleaned up and no longer exists on the server
 
     Ping,
     Pong,
