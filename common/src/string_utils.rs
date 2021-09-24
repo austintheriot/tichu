45427e97cf0ad5d1b3221757cc864a09dc47a727
Austin Theriot <austinmtheriot@gmail.com>
2021-09-23 20:04:15 -0500

    fix unimplemented in comm on lib

--- common/src/string_utils.rs
+++ common/src/string_utils.rs
@@ -4,7 +4,7 @@ use uuid::Uuid;
 
 fn get_random_string_of_len(len: usize) -> String {
     let random_name = Uuid::new_v4().to_string();
-    let mut random_name = random_name.graphemes(true).collect::<Vec<&str>>().split;
+    let mut random_name = random_name.graphemes(true).collect::<Vec<&str>>();
     random_name.truncate(len);
     let random_name = random_name.join("");
     random_name.to_uppercase()
