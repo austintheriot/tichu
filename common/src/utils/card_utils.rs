4829a8341580cb6917e2361c98e0eb3167f43eaa
Austin Theriot <austinmtheriot@gmail.com>
2021-10-25 19:56:39 -0500

    implemnting get valid combos

--- common/src/utils/card_utils.rs
+++ common/src/utils/card_utils.rs
@@ -1,4 +1,4 @@
-use crate::{Card, Pair, Single, ValidCardCombos};
+use crate::{Card, Pair, Single, Trio, ValidCardCombos};
 
 // TODO: account for single special cards and Phoenix wild card
 pub fn get_card_combination(cards: &Vec<Card>) -> Option<ValidCardCombos> {
@@ -32,6 +32,20 @@ pub fn get_card_combination(cards: &Vec<Card>) -> Option<ValidCardCombos> {
     }
 
     // length 3: a trio of cards of equal rank
+    if cards.len() == 3 {
+        if let [card_0, card_1, card_2] = &cards[..cards.len()] {
+            return if card_0.value == card_1.value && card_1.value == card_2.value {
+                Some(ValidCardCombos::Trio(Trio {
+                    cards: cards.clone(),
+                    value: card_0.value.clone(),
+                }))
+            } else {
+                None
+            };
+        }
+
+        return None;
+    }
 
     // length 4:
     // a bomb (4 of the same)
@@ -51,29 +65,186 @@ pub fn get_card_combination(cards: &Vec<Card>) -> Option<ValidCardCombos> {
 
 #[cfg(test)]
 mod tests {
+    use crate::{get_card_combination, Card, CardSuit, CardValue};
+
     mod test_get_random_string_of_len {
         use super::super::get_card_combination;
-        #[test]
-        fn it_should_produce_intended_length() {
-            // no cards
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+        use crate::{Card, CardSuit, CardValue, Pair, Single, Trio, ValidCardCombos};
 
+        #[test]
+        fn it_should_return_some_for_correct_combos() {
             // a single card
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![Card {
+                        suit: CardSuit::Sword,
+                        value: CardValue(2),
+                    }])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Single(Single(Card {
+                    suit: CardSuit::Sword,
+                    value: CardValue(2),
+                })))
+            );
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![Card {
+                        suit: CardSuit::Pagoda,
+                        value: CardValue(14),
+                    }])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Single(Single(Card {
+                    suit: CardSuit::Pagoda,
+                    value: CardValue(14),
+                })))
+            );
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![Card {
+                        suit: CardSuit::Dragon,
+                        value: CardValue::noop(),
+                    }])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Single(Single(Card {
+                    suit: CardSuit::Dragon,
+                    value: CardValue::noop(),
+                })))
+            );
 
             // a pair of cards of equal rank
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-
-            // a sequence of pairs of adjacent value
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![
+                        Card {
+                            suit: CardSuit::Sword,
+                            value: CardValue(7),
+                        },
+                        Card {
+                            suit: CardSuit::Jade,
+                            value: CardValue(7),
+                        },
+                    ])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Pair(Pair {
+                    value: CardValue(7),
+                    cards: vec![ /* omitted */],
+                }))
+            );
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![
+                        Card {
+                            suit: CardSuit::Star,
+                            value: CardValue(13),
+                        },
+                        Card {
+                            suit: CardSuit::Pagoda,
+                            value: CardValue(13),
+                        },
+                    ])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Pair(Pair {
+                    value: CardValue(13),
+                    cards: vec![ /* omitted */],
+                }))
+            );
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![
+                        Card {
+                            suit: CardSuit::Jade,
+                            value: CardValue(14),
+                        },
+                        Card {
+                            suit: CardSuit::Pagoda,
+                            value: CardValue(14),
+                        },
+                    ])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Pair(Pair {
+                    value: CardValue(14),
+                    cards: vec![ /* omitted */],
+                }))
+            );
 
             // a trio of cards of equal rank
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![
+                        Card {
+                            suit: CardSuit::Jade,
+                            value: CardValue(3),
+                        },
+                        Card {
+                            suit: CardSuit::Pagoda,
+                            value: CardValue(3),
+                        },
+                        Card {
+                            suit: CardSuit::Star,
+                            value: CardValue(3),
+                        },
+                    ])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Trio(Trio {
+                    value: CardValue(3),
+                    cards: vec![ /* omitted */],
+                }))
+            );
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![
+                        Card {
+                            suit: CardSuit::Star,
+                            value: CardValue(11),
+                        },
+                        Card {
+                            suit: CardSuit::Sword,
+                            value: CardValue(11),
+                        },
+                        Card {
+                            suit: CardSuit::Jade,
+                            value: CardValue(11),
+                        },
+                    ])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Trio(Trio {
+                    value: CardValue(11),
+                    cards: vec![ /* omitted */],
+                }))
+            );
+            assert_eq!(
+                std::mem::discriminant(
+                    &get_card_combination(&vec![
+                        Card {
+                            suit: CardSuit::Pagoda,
+                            value: CardValue(14),
+                        },
+                        Card {
+                            suit: CardSuit::Sword,
+                            value: CardValue(14),
+                        },
+                        Card {
+                            suit: CardSuit::Jade,
+                            value: CardValue(14),
+                        },
+                    ])
+                    .unwrap()
+                ),
+                std::mem::discriminant(&ValidCardCombos::Trio(Trio {
+                    value: CardValue(14),
+                    cards: vec![ /* omitted */],
+                }))
+            );
+
+            // a sequence of pairs of adjacent value
             assert_eq!(get_card_combination(&vec![]).is_some(), false);
             assert_eq!(get_card_combination(&vec![]).is_some(), false);
             assert_eq!(get_card_combination(&vec![]).is_some(), false);
@@ -93,11 +264,141 @@ mod tests {
             assert_eq!(get_card_combination(&vec![]).is_some(), false);
             assert_eq!(get_card_combination(&vec![]).is_some(), false);
 
+            // sequence bomb
+            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+
+            // sequence of pairs of adjacent value (any length)
+            // length 4
+            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+
+            // length 6
+            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+
+            // length 8
+            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+
+            // length 10
+            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+
+            // TODO: every combo as possible with a Phoenix:
+            // length 2: a pair of cards of equal rank
+            // OR 1 standard card and 1 Phoenix
+
+            // length 3: a trio of cards of equal rank
+
+            // length 4:
+            // a bomb (4 of the same)
+            // a sequence of pairs of adjacent value
+
+            // length 5:
+            // a full house (trio + pair)
             // a sequence of length at least 5
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
-            assert_eq!(get_card_combination(&vec![]).is_some(), false);
+            // a bomb (sequence of 5, all same suit)
+
+            // any length greater than 5:
+            // a sequence
+            // a sequence of pairs of adjacent value
         }
+    }
+
+    #[test]
+    fn it_should_return_none_for_bogus_combos() {
+        assert_eq!(get_card_combination(&vec![]), None);
+
+        // two different cards
+        assert_eq!(
+            get_card_combination(&vec![
+                Card {
+                    suit: CardSuit::Pagoda,
+                    value: CardValue(2),
+                },
+                Card {
+                    suit: CardSuit::Star,
+                    value: CardValue(3),
+                }
+            ]),
+            None
+        );
+
+        // non-Phoenix special card and standard card:
+        assert_eq!(
+            get_card_combination(&vec![
+                Card {
+                    suit: CardSuit::Dog,
+                    value: CardValue::noop(),
+                },
+                Card {
+                    suit: CardSuit::Star,
+                    value: CardValue(11),
+                }
+            ]),
+            None
+        );
+        assert_eq!(
+            get_card_combination(&vec![
+                Card {
+                    suit: CardSuit::MahJong,
+                    value: CardValue::noop(),
+                },
+                Card {
+                    suit: CardSuit::Jade,
+                    value: CardValue(6),
+                }
+            ]),
+            None
+        );
+        assert_eq!(
+            get_card_combination(&vec![
+                Card {
+                    suit: CardSuit::Dragon,
+                    value: CardValue::noop(),
+                },
+                Card {
+                    suit: CardSuit::Pagoda,
+                    value: CardValue(14),
+                }
+            ]),
+            None
+        );
+
+        // 3: 2 same value and 3rd non-matching
+        assert_eq!(
+            get_card_combination(&vec![
+                Card {
+                    suit: CardSuit::Pagoda,
+                    value: CardValue(2),
+                },
+                Card {
+                    suit: CardSuit::Sword,
+                    value: CardValue(2),
+                },
+                Card {
+                    suit: CardSuit::Jade,
+                    value: CardValue(5),
+                }
+            ]),
+            None
+        );
+        // 3: 3 different values
+        assert_eq!(
+            get_card_combination(&vec![
+                Card {
+                    suit: CardSuit::Pagoda,
+                    value: CardValue(2),
+                },
+                Card {
+                    suit: CardSuit::Sword,
+                    value: CardValue(3),
+                },
+                Card {
+                    suit: CardSuit::Jade,
+                    value: CardValue(4),
+                }
+            ]),
+            None
+        );
     }
 }
 
