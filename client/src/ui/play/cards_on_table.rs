d496e976c641aa63240d2845011aa1690f40eca5
Austin Theriot <austinmtheriot@gmail.com>
2022-02-22 08:32:13 -0600

    styling WIP

--- client/src/ui/play/cards_on_table.rs
+++ client/src/ui/play/cards_on_table.rs
@@ -12,20 +12,24 @@ pub fn cards_on_table() -> Html {
             let last_combo = play_state.table.last();
             return if let Some(last_combo) = last_combo {
                 html! {
-                        <>
-                            <p>{"Cards on table:"}</p>
-                            <ul>
-                                {for last_combo.cards().iter().map(|card| {
-                                    html!{
-                                        <li>
-                                            <p>
-                                                {&format!("{}", card)}
-                                            </p>
-                                        </li>
-                                }
-                            })}
-                            </ul>
-                       </>
+                    <ul class="cards-on-table">
+                        {for last_combo.cards().iter().map(|card| {
+                            html!{
+                                <li>
+                                    if card.suit.is_special() {
+                                    <span class="special">{card.suit.icon()}</span>
+                                    } else {
+                                        <span class="regular-value">
+                                            {card.value.icon()}
+                                        </span>
+                                        <span class="regular-suit">
+                                            {card.suit.icon()}
+                                        </span>
+                                    }
+                                </li>
+                        }
+                    })}
+                    </ul>
                 }
             } else {
                 html! {  <p>{"No cards on table yet"}</p> }
