0d1ba6721c6333508e609e7201d53846298e4d02
Austin Theriot <austinmtheriot@gmail.com>
2022-12-16 20:47:35 -0600

    remove wee allocator

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -5,11 +5,6 @@ mod global;
 mod ui;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 // This is the entry point for the web app
 #[wasm_bindgen]
 pub fn run_app() -> Result<(), JsValue> {
